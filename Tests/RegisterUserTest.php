7e16ae24b905a9e51cf71d81b78a8dd127fb562d
Ayoze Vera <ayo.verad@gmail.com>
2016-09-29 18:15:58 +0100

    test done

---
+++ Tests/RegisterUserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Modules\RestSocialLogin\Tests\TestCase;
+
+class RegisterUserTest extends TestCase
+{
+  /**
+   * A successfull user-password register Test.
+   *
+   * @return void
+   */
+  public function testSuccess()
+  {
+    $params = ['email' => 'paco@email.com', 'password' => 'secret'];
+
+    $this->post('/auth/register', $params)->dump();
+
+    $this->assertResponseStatus(200);
+    $this->seeInDatabase('users', ['email' => $params->email]);
+    $this->seeJsonStructure(['token', 'user']);
+  }
+}
